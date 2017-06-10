3835beedb9980f75da061fb3df036d7fc55d016f
OdooPlay <odooplay@gmail.com>
2017-06-10 20:32:03 +0200

    is working the reset and decrease

--- app/src/main/java/com/blogspot/odooplay/android_00018_contador/MainActivity.java
+++ app/src/main/java/com/blogspot/odooplay/android_00018_contador/MainActivity.java
@@ -1,5 +1,6 @@
 package com.blogspot.odooplay.android_00018_contador;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,45 +9,34 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends Activity {
+    public int contador;
+    TextView textoResultado;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        textoResultado=(TextView)findViewById(R.id.contadorTexto);
+        contador=0;
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    public void incrementaContador(View vista){
+        contador++;
+        mostrarResultado();
     }
+    public void restaContador(View vista){
+        contador--;
+        mostrarResultado();
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
+    public void reseteaContador(View vista){
+        contador=0;
+        mostrarResultado();
+
+    }
+    public void mostrarResultado(){
+        TextView textoResultado=(TextView)findViewById(R.id.contadorTexto);
+        textoResultado.setText("" + contador); }
 }
